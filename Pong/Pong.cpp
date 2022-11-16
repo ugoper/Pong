3813cc860f7b0667c76768402d4e6f87a76d2643
Ugo Peressoni <ugo.peressoni@gmail.com>
2022-11-16 21:38:23 +0100

    Arka funzionante

--- Pong/Pong.cpp
+++ Pong/Pong.cpp
@@ -17,9 +17,108 @@ using std::string;
 #include "basewin.h" // include la classe BaseWindow
 #include "framework.h"
 #include "Pong.h"
+// definisce la classe della pallina
 
 // contiene le dimensioni della finestra
 float winX=0, winY=0;
+
+//definisce l'arka
+
+class arka {
+public:
+    SYSTEMTIME tempo = { 0 };
+    D2D1_RECT_F aspetto{ 0,0,0,0 };
+    int velX = 5;
+    int velY = 5;
+    int score = 0;
+
+    int muovi(int direzione) {
+        // movimento se la direzione è verso l'alto
+        if (direzione > 0) {
+            aspetto.bottom -= velX;
+            aspetto.top -= velX;
+            // controllo che non sia arrivato al bordo
+            float bordoYa = winY / 25;
+            if (aspetto.top < bordoYa) {
+                aspetto.bottom += velX;
+                aspetto.top += velX;
+            }
+        }
+        else {
+            // movimento se la direzione è verso il basso
+            aspetto.bottom += velX;
+            aspetto.top += velX;
+            // controllo che non sia arrivato al bordo
+            float bordoYb = winY * 94 / 100;
+            if (aspetto.bottom > bordoYb) {
+                aspetto.bottom -= velX;
+                aspetto.top -= velX;
+            }
+        }
+        return 1;
+    }
+
+} arka1;
+
+// definisce la struttura della palla
+class Palla {
+public:
+    float diam=5;
+    ULONGLONG tempo = { 0};
+    D2D1_ELLIPSE p = { 0,0,0,0 };
+    D2D1_POINT_2F c = { 0,0 };
+    int velX = 0; // da settare a 5
+    int velY=0;
+
+    int muovi() {
+        ULONGLONG time;
+        time = GetTickCount64();
+
+        if (tempo == 0)
+        {
+            tempo = time;
+        }
+        else {
+            // calcola il delta tempo
+            FLOAT delta = float ((time - tempo)/10);
+            //  muove la pallina in X
+            c.x += delta * velX;
+            float bordoXd = winX-diam;
+            float bordoXs = winX/40 + diam;
+            if (c.x > bordoXd) {
+                c.x = 2 * bordoXd - c.x;
+                velX = -velX;
+            }
+            if (c.x < bordoXs) {
+                // controllo che arka sia in posizione
+                if ((c.y >= arka1.aspetto.top) && (c.y <= arka1.aspetto.bottom)) {
+                    c.x = 2 * bordoXs - c.x;
+                    velX = -velX;
+                }
+                else {
+                    // arka in posizione sbagliata
+                    arka1.score -= 100;
+                    return -1;
+                }
+            }
+
+            // muove la pallina in Y
+            c.y += delta * velY;
+            float bordoYb = winY * 94 / 100 - diam;
+            float bordoYa = winY / 25+diam;
+            if (c.y > bordoYb) {
+                c.y = 2 * bordoYb - c.y;
+                velY = -velY;
+            }
+            if (c.y < bordoYa) {
+                c.y = 2 * bordoYa - c.y;
+                velY = -velY;
+            }
+            tempo = time;
+        }
+        return 0;
+    }
+} palla1;
 
 template <class T> void SafeRelease(T** ppT) // template per rilasciare puntatori dalla heap
 {
@@ -37,6 +136,8 @@ class MainWindow : public BaseWindow<MainWindow>  // definisce class MainWindow 
     ID2D1HwndRenderTarget* pRenderTarget; // Interfaccia ID2D1HwndRenderTarget (d2d1.h), Esegue il rendering delle istruzioni di disegno in una finestra
     ID2D1SolidColorBrush* pBrush; // Interfaccia ID2D1SolidColorBrush (d2d1.h), Disegna un'area con un colore a tinta unita
     IDWriteTextFormat* pTextF;
+    HANDLE  m_hTimer=nullptr; // handle del timer
+
 
     D2D1_RECT_F highBorder={0,0,0,0}, lowBorder = { 0,0,0,0 }, userBar = { 0,0,0,0 };
 
@@ -44,13 +145,16 @@ class MainWindow : public BaseWindow<MainWindow>  // definisce class MainWindow 
     HRESULT CreateGraphicsResources();
     void    DiscardGraphicsResources();
     void    OnPaint();
-    void    Resize();
+    void    Resize(); 
+    BOOL    InitializeTimer();
 
 public:
 
     MainWindow() : pFactory(NULL), pWriteFactory(NULL), pRenderTarget(NULL), pBrush(NULL), pTextF(NULL)
     {
     }
+
+    void    WaitTimer();
 
     PCWSTR  ClassName() const { return L"Circle Window Class"; }
     LRESULT HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam);
@@ -64,6 +168,10 @@ void MainWindow::CalculateLayout() // draw the screen
     {
         highBorder = D2D1::RectF(0, winY/25, winX, winY/20);
         lowBorder = D2D1::RectF(0, winY *94 / 100, winX, winY);
+        arka1.aspetto = D2D1::RectF(winX/100, winY * 9/20, winX/40, winY*11/20);
+        palla1.c.x = winX / 40 + palla1.diam;
+        palla1.c.y = winY * 10 / 20;
+        palla1.p = D2D1::Ellipse(palla1.c, palla1.diam, palla1.diam);
     }
 }
 
@@ -112,21 +220,43 @@ void MainWindow::OnPaint()
     {
         PAINTSTRUCT ps;
         WCHAR sc_testo[100] = L"Score";
+
         int lungh = 0;
        
         BeginPaint(m_hwnd, &ps);
 
         pRenderTarget->BeginDraw();
+        // nuove la palla
+        if (palla1.velX == 0) {
+            lungh = swprintf_s(sc_testo, 100, L"PREMI Q PER INIZIARE");
+            pRenderTarget->DrawTextW(sc_testo, lungh, pTextF, D2D1::RectF(0, 0, winX, 100), pBrush);
 
-        // imposta lo sfondo nero
-        pRenderTarget->Clear(D2D1::ColorF(D2D1::ColorF::Black));
-        // disegna la barra superiore
-        pRenderTarget->FillRectangle(highBorder, pBrush);
-        // disegna la barra inferiore
-        pRenderTarget->FillRectangle(lowBorder, pBrush);
-        // scrive il testo sulla barra superiore
-        lungh = swprintf_s(sc_testo, 100, L"Cord X %0.f - Cord Y  %0.f - Score", winX, winY);
-        pRenderTarget->DrawTextW(sc_testo, lungh,pTextF, D2D1::RectF(0, 0, winX, 100),pBrush);
+        }
+        else {
+            if (palla1.muovi() >= 0) {
+                palla1.p = D2D1::Ellipse(palla1.c, palla1.diam, palla1.diam);
+
+                // imposta lo sfondo nero
+                pRenderTarget->Clear(D2D1::ColorF(D2D1::ColorF::Black));
+                // disegna la barra superiore
+                pRenderTarget->FillRectangle(highBorder, pBrush);
+                // disegna la barra inferiore
+                pRenderTarget->FillRectangle(lowBorder, pBrush);
+                // disegna la palla
+                pRenderTarget->FillEllipse(palla1.p, pBrush);
+                // disegna arka
+                pRenderTarget->FillRectangle(arka1.aspetto, pBrush);
+                // scrive il testo sulla barra superiore
+                lungh = swprintf_s(sc_testo, 100, L"Cord X %0.f - Cord Y  %0.f - Score %d", winX, winY, arka1.score);
+                pRenderTarget->DrawTextW(sc_testo, lungh, pTextF, D2D1::RectF(0, 0, winX, 100), pBrush);
+            }
+            else {
+                lungh = swprintf_s(sc_testo, 100, L"Cord X %0.f - Cord Y  %0.f - Score %d  GAME OVER", winX, winY, arka1.score);
+                pRenderTarget->DrawTextW(sc_testo, lungh, pTextF, D2D1::RectF(0, 0, winX, 100), pBrush);
+                palla1.velX = 0;
+                palla1.velY = 0;
+            }
+        }
 
         hr = pRenderTarget->EndDraw();
         if (FAILED(hr) || hr == D2DERR_RECREATE_TARGET)
@@ -168,11 +298,17 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE, _In_ PWSTR, _I
     // Run the message loop.
 
     MSG msg = { };
-    while (GetMessage(&msg, NULL, 0, 0))
+    while (msg.message != WM_QUIT)
     {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
+        if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+            continue;
+        }
+        win.WaitTimer();
     }
+
 
     return 0;
 }
@@ -187,10 +323,15 @@ LRESULT MainWindow::HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam)
         {
             return -1;  // Fail CreateWindowEx.
         }
+        if (!InitializeTimer())
+        {
+            return -1;
+        }
         return 0;
 
     case WM_DESTROY:
         DiscardGraphicsResources();
+        CloseHandle(m_hTimer);
         SafeRelease(&pFactory);
         PostQuitMessage(0);
         return 0;
@@ -199,11 +340,52 @@ LRESULT MainWindow::HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam)
         OnPaint();
         return 0;
 
+    case WM_CHAR:
+        if (wParam == 'w')
+            arka1.muovi(1);
+        if (wParam == 's')
+            arka1.muovi(-1);
+        if (wParam == 'q') {
+            palla1.velX = 5;
+            palla1.velY = 5;
 
+        }
+
+        return 0;
 
     case WM_SIZE:
         Resize();
         return 0;
     }
     return DefWindowProc(m_hwnd, uMsg, wParam, lParam);
+}
+
+BOOL MainWindow::InitializeTimer()
+{
+    m_hTimer = CreateWaitableTimer(NULL, FALSE, NULL);
+    if (m_hTimer == NULL)
+    {
+        return FALSE;
+    }
+
+    LARGE_INTEGER li = { 0 };
+
+    if (!SetWaitableTimer(m_hTimer, &li, (1000/80), NULL, NULL, FALSE))
+    {
+        CloseHandle(m_hTimer);
+        m_hTimer = NULL;
+        return FALSE;
+    }
+
+    return TRUE;
+}
+
+void MainWindow::WaitTimer()
+{
+    // Wait until the timer expires or any message is posted.
+    if (MsgWaitForMultipleObjects(1, &m_hTimer, FALSE, INFINITE, QS_ALLINPUT)
+        == WAIT_OBJECT_0)
+    {
+        InvalidateRect(m_hwnd, NULL, FALSE);
+    }
 }
